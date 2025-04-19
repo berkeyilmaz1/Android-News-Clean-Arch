e843482167d11071726c78a80684a75d11a9ef20
berke.yz <berkeyilmaz189@gmail.com>
2025-04-19 20:43:30 +0300

    System UI Controller added

--- app/src/main/java/com/duzceders/newscleanarch/MainActivity.kt
+++ app/src/main/java/com/duzceders/newscleanarch/MainActivity.kt
@@ -6,36 +6,50 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.duzceders.newscleanarch.presentation.navgraph.NavGraph
 import com.duzceders.newscleanarch.ui.theme.NewsCleanArchTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-   val viewModel by viewModels<MainViewModel>()
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
-        installSplashScreen().apply{
-            setKeepOnScreenCondition{
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
                 viewModel.splashCondition
             }
         }
 
         setContent {
+
+            val isSystemInDarkMode = isSystemInDarkTheme()
+            val systemController = rememberSystemUiController()
+
+            SideEffect {
+                systemController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = !isSystemInDarkMode
+                )
+            }
             NewsCleanArchTheme(
                 dynamicColor = false
             ) {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    val startDestination=viewModel.startDestination
+                    val startDestination = viewModel.startDestination
                     NavGraph(startDestination)
                 }
             }
